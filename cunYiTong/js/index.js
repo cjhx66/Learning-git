e65fa3b8618b3adc0363150974287d032aa1ca22
cjhx <cjhx_ymm@163.com>
2019-03-15 10:43:14 +0800

    swiper

--- cunYiTong/js/index.js
+++ cunYiTong/js/index.js
@@ -19,75 +19,75 @@ function load() {
         oImgW = oImg[i].width = carousel.offsetWidth;
     }
     tu.style.width = (oImgW) * (oImg.length) + "px";
-    function move(index) {
-        for (var i = 0; i < oImg.length; i++) {
-            oImg[i].index = i;
-            oImg[i].style.left = 0;
-        }
-//        ÉèÖÃÔ²µã
-        for (var j = 0; j < yuan.length; j++) {
-            yuan[j].style.background = "white";
-            yuan[j].style.width = "6px";
-        }
-        getMove(tu, {left: -(oImg[0].offsetWidth * index)});
-        if (index == 4) {
-            yuan[3].style.background = "#00B38C";
-            yuan[3].style.width = "12px";
-        } else {
-            yuan[index].style.background = "#00d38c";
-            yuan[index].style.width = "12px";
-        }
-    }
+//     function move(index) {
+//         for (var i = 0; i < oImg.length; i++) {
+//             oImg[i].index = i;
+//             oImg[i].style.left = 0;
+//         }
+// //        ï¿½ï¿½ï¿½ï¿½Ô²ï¿½ï¿½
+//         for (var j = 0; j < yuan.length; j++) {
+//             yuan[j].style.background = "white";
+//             yuan[j].style.width = "6px";
+//         }
+//         getMove(tu, {left: -(oImg[0].offsetWidth * index)});
+//         if (index == 4) {
+//             yuan[3].style.background = "#00B38C";
+//             yuan[3].style.width = "12px";
+//         } else {
+//             yuan[index].style.background = "#00d38c";
+//             yuan[index].style.width = "12px";
+//         }
+//     }
 
-    move(0);
-    var num = 1;
+//     move(0);
+//     var num = 1;
 
-    function jiSu() {
-        if (num == oImg.length) {
-            num = 0;
-            tu.style.left = 0;
-        }
-        move(num);
-        num++;
-    }
+//     function jiSu() {
+//         if (num == oImg.length) {
+//             num = 0;
+//             tu.style.left = 0;
+//         }
+//         move(num);
+//         num++;
+//     }
 
-    var time = setInterval(jiSu, 2000);
-    left.onclick = function () {
-        clearInterval(time);
-        num--;
-        if (num == -1) {
-            num = oImg.length - 2;
-            tu.style.left = -(oImg.length - 1) * oImg[0].offsetWidth + "px";
-        }
-        move(num);
-        time = setInterval('jiSu', 2000);
-    };
-    right.onclick = function () {
-        clearInterval(time);
-        jiSu();
-        time = setInterval('jiSu', 2000);
-    };
-    for (var j = 0; j < yuan.length; j++) {
-        yuan[j].onmouseover = function (event) {
-            clearInterval(time);
-//            »ñÈ¡¶ÔÓ¦Ô­µãµÄ×Ô¶¨ÒåÊôÐÔ
-            num = event.target.dataset.index;
-            move(num);
-            time = setInterval('jiSu', 2000);
-        }
-    }
-    tu.onmousemove = function () {
-        clearInterval(time);
-    }
-    tu.onmouseout = function () {
-        time = setInterval(jiSu, 2000);
-    }
+//     var time = setInterval(jiSu, 2000);
+//     left.onclick = function () {
+//         clearInterval(time);
+//         num--;
+//         if (num == -1) {
+//             num = oImg.length - 2;
+//             tu.style.left = -(oImg.length - 1) * oImg[0].offsetWidth + "px";
+//         }
+//         move(num);
+//         time = setInterval('jiSu', 2000);
+//     };
+//     right.onclick = function () {
+//         clearInterval(time);
+//         jiSu();
+//         time = setInterval('jiSu', 2000);
+//     };
+//     for (var j = 0; j < yuan.length; j++) {
+//         yuan[j].onmouseover = function (event) {
+//             clearInterval(time);
+// //            ï¿½ï¿½È¡ï¿½ï¿½Ó¦Ô­ï¿½ï¿½ï¿½ï¿½Ô¶ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+//             num = event.target.dataset.index;
+//             move(num);
+//             time = setInterval('jiSu', 2000);
+//         }
+//     }
+//     tu.onmousemove = function () {
+//         clearInterval(time);
+//     }
+//     tu.onmouseout = function () {
+//         time = setInterval(jiSu, 2000);
+//     }
 
-    //Ê¡ÂÔºÅ
-    var artel = document.getElementsByClassName("artel-left-right-content");
-    var index = 0;
-    for (var j = 0; j < artel.length; j++) {
-        index = artel[j];
-        $clamp(index, {clamp: 3, useNativeClamp: false});
-    }
+//     //Ê¡ï¿½Ôºï¿½
+//     var artel = document.getElementsByClassName("artel-left-right-content");
+//     var index = 0;
+//     for (var j = 0; j < artel.length; j++) {
+//         index = artel[j];
+//         $clamp(index, {clamp: 3, useNativeClamp: false});
+//     }
 }
